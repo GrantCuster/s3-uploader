42d3a62d971c3a63a4e20f5862d4caef33409150
Grant Custer <grantcuster@gmail.com>
2024-06-08 13:37:58 +0000

    initial

---
+++ server.js
@@ -0,0 +1,176 @@
+const express = require("express");
+const sharp = require("sharp");
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+const ViteExpress = require("vite-express");
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+
+app.get("/message", (_, res) => res.send("Hello from express!"));
+
+// Set up AWS S3
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const formatDate = () => {
+  const now = new Date();
+  return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(
+    2,
+    "0",
+  )}-${String(now.getDate()).padStart(2, "0")}-${String(
+    now.getHours(),
+  ).padStart(2, "0")}-${String(now.getMinutes()).padStart(2, "0")}-${String(
+    now.getSeconds(),
+  ).padStart(2, "0")}`;
+};
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function(req, file, cb) {
+    const formattedDate = formatDate();
+    cb(null, formattedDate + path.extname(file.originalname)); // Append the file extension
+  },
+});
+const upload = multer({ storage: storage });
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
+const smallSize = 800;
+const largeSize = 2000;
+
+const resizeImage = (inputPath, outputDir, callback) => {
+  const fileName = path.basename(inputPath, path.extname(inputPath));
+
+  // Define output paths
+  const smallPath = path.join(outputDir, `${fileName}-${smallSize}.jpg`);
+  const largePath = path.join(outputDir, `${fileName}-${largeSize}.jpg`);
+
+  // Resize to small (512px)
+  sharp(inputPath)
+    .resize(smallSize, smallSize, {
+      fit: "inside",
+      withoutEnlargement: true,
+    })
+    .toFile(smallPath, (err) => {
+      if (err) {
+        return callback(err);
+      }
+
+      // Resize to large (1600px)
+      sharp(inputPath)
+        .resize(largeSize, largeSize, {
+          fit: "inside",
+          withoutEnlargement: true,
+        })
+        .toFile(largePath, (err) => {
+          if (err) {
+            return callback(err);
+          }
+
+          callback(null, {
+            small: smallPath,
+            large: largePath,
+          });
+        });
+    });
+};
+
+const uploadToS3 = (filePath, key, callback) => {
+  const fileContent = fs.readFileSync(filePath);
+
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key,
+    Body: fileContent,
+    ContentType: "image/jpeg",
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, data.Location);
+  });
+};
+
+app.post("/api/upload", upload.single("image"), (req, res) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send("No file uploaded.");
+  }
+
+  const outputDir = "uploads/";
+  resizeImage(file.path, outputDir, (err, resizedImages) => {
+    if (err) {
+      console.error("Error resizing image:", err);
+      return res.status(500).send("Error resizing image.");
+    }
+
+    const { small, large } = resizedImages;
+    const fileName = path.basename(file.path, path.extname(file.path));
+
+    // Upload large version
+    uploadToS3(large, `${fileName}-${largeSize}.jpg`, (err, smallLocation) => {
+      if (err) {
+        console.error("Error uploading large image:", err);
+        return res.status(500).send("Error uploading large image.");
+      }
+
+      // Upload small version
+      uploadToS3(
+        small,
+        `${fileName}-${smallSize}.jpg`,
+        (err, largeLocation) => {
+          if (err) {
+            console.error("Error uploading small image:", err);
+            return res.status(500).send("Error uploading small image.");
+          }
+
+          // Clean up local files
+          fs.unlinkSync(file.path);
+          fs.unlinkSync(small);
+          fs.unlinkSync(large);
+
+          res.send({
+            message: "Files uploaded successfully",
+            smallImageUrl: smallLocation,
+            largeImageUrl: largeLocation,
+          });
+        },
+      );
+    });
+  });
+});
+
+app.get("/api/list-objects", async (req, res) => {
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+  };
+
+  try {
+    const data = await s3.listObjectsV2(params).promise();
+    const sortedData = data.Contents.sort(
+      (a, b) => new Date(b.LastModified) - new Date(a.LastModified),
+    );
+    res.json(sortedData);
+  } catch (err) {
+    console.error("Error listing objects:", err);
+    res.status(500).send("Error listing objects.");
+  }
+});
+
+const port = process.env.NODE_ENV === "production" ? 8080 : 3000;
+ViteExpress.listen(app, port, () => console.log("Server is listening..."));
---
+++ src/App.tsx
@@ -0,0 +1,155 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [bump, setBump] = useState(0);
+  return (
+    <div
+      style={{
+        margin: "16px auto",
+        maxWidth: "800px",
+      }}
+    >
+      <UploadImage setBump={setBump} />
+      <BucketContents bump={bump} />
+    </div>
+  );
+}
+
+const UploadImage = ({
+  setBump,
+}: {
+  setBump: Dispatch<SetStateAction<number>>;
+}) => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploadStatus, setUploadStatus] = useState<string>("");
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      setUploadStatus("No file selected.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    try {
+      await axios.post("http://localhost:3000/api/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      setUploadStatus(`File uploaded successfully.`);
+      setBump((prev) => prev + 1);
+    } catch (error) {
+      // @ts-ignore
+      setUploadStatus(`Error uploading file: ${error.message}`);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Upload Image</h1>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {uploadStatus && (
+        <>
+          <p>{uploadStatus}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+const formatBytes = (bytes: number, decimals = 2) => {
+  if (bytes === 0) return "0 Bytes";
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+};
+
+const BucketContents = ({ bump }: { bump: number }) => {
+  bump; // to silence the unused variable warning
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      try {
+        const response = await fetch("/api/list-objects");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setFiles(data);
+      } catch (error) {
+        console.error("Error fetching files:", error);
+      }
+    };
+
+    fetchFiles();
+  }, [bump]);
+
+  return (
+    <div>
+      <h1>Bucket Contents</h1>
+      <div>
+        {files.slice(0,20).map((file, index) => (
+          <div
+            key={index}
+            style={{
+              width: "full",
+            }}
+          >
+            {file.Key.includes("-800.") ? (
+              <div>
+                <img
+                  src={`https://grant-uploader.s3.amazonaws.com/${file.Key}`}
+                  style={{ maxWidth: "400px", marginTop: "48px" }}
+                  alt="800"
+                />
+              </div>
+            ) : null}
+            <div style={{ display: "flex", width: "full" }}>
+              <input
+                type="text"
+                style={{
+                  width: "100%",
+                  backgroundColor: "transparent",
+                  color: "inherit",
+                  border: "var(--gray) 1px solid",
+                  fontSize: "16px",
+                }}
+                value={file.Key}
+                onFocus={(e) => e.target.select()}
+              />
+              <a
+                style={{ display: "block", width: "100%" }}
+                href={`https://grant-uploader.s3.amazonaws.com/${file.Key}`}
+                rel="noopener noreferrer"
+              >
+                {file.Key}
+              </a>
+            </div>
+            <div style={{ display: "flex" }}>
+              <div style={{ width: "100%" }}>{formatBytes(file.Size)}</div>
+              <div style={{ width: "100%" }}>
+                {new Date(file.LastModified).toLocaleString()}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
